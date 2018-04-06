8193c5429e0b3c12b1d8f965542d2e7cfb0bd570
Nolan <nolanehs@gmail.com>
2018-04-06 08:55:45 -0400

    def new, create, and show in UserController

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,13 +1,20 @@
 class UserController < ApplicationController
 
-  def create
-    @user = user.new(params[:id])
-      if user.save
-        session[:user_id] = @user.id
-        redirect to user_path(@user)
-      end
+def new
+  @user = User.new
+end
+
+
+def create
+  @user = user.new(params[:id])
+  if user.save
+    session[:user_id] = @user.id
+    redirect to user_path(@user)
   end
+end
 
-
+def show
+  @user = User.find(params[:id])
+end
 
 end
---
+++ db/migrate/20180405161451_opportunities.rb
@@ -0,0 +1,7 @@
+class Opportunities < ActiveRecord::Migration[5.1]
+  def change
+    create_table :opportunities do |t|
+      t.string :
+
+  end
+end
