574e52cb4cbedbf5497eb9d796bc88ba5d79cbb2
Nolan <nolanehs@gmail.com>
2018-04-06 10:13:08 -0400

    Added columns to opportunities table

--- db/migrate/20180405161451_opportunities.rb
+++ db/migrate/20180405161451_opportunities.rb
@@ -1,7 +1,9 @@
 class Opportunities < ActiveRecord::Migration[5.1]
   def change
     create_table :opportunities do |t|
-      t.string :
-
+      t.integer :category_id
+      t.string :description
+      t.string :title
+      
   end
 end
