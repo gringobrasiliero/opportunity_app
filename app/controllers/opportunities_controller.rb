152193a86a0470de90145721f0115bd06562f9e5
Nolan <nolanehs@gmail.com>
2018-04-08 07:31:32 -0400

    defined opportunity_params; defined index, show, new in opportunity controller

--- app/controllers/opportunities_controller.rb
+++ app/controllers/opportunities_controller.rb
@@ -1,2 +1,19 @@
 class OpportunitiesController < ApplicationController
+
+def index
+@opportunities = Opportuity.all
+end
+
+def show
+@Opportunity = Opportunity.find(opportunity_params)
+end
+
+def new
+@Opportunity = Opportunity.new(opportunity_params)
+end
+
+
+
+def opportunity_params
+  params.require(:opportunity).permit(:title, :description)
 end
